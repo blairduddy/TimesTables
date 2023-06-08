5ec02f4b182d79356cf6e254eeed958a2dc3039d
Blair Duddy <blairduddy@hotmail.co.uk>
2023-06-08 16:09:11 -0400

    Initial build, basic UI layout, no logic

--- TimesTables/ContentView.swift
+++ TimesTables/ContentView.swift
@@ -8,14 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isActive = false
+    @State private var finalAlert = false
+    @State private var userSelection = 2
+    @State private var selectedNumberofQuestions = 5
+    @State private var userAnswer = ""
+    
+    let numOfQuestions = [5, 10, 15, 20]
+    let questionNumber = [Int]()
+    let question = [Int]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section {
+                    Text("Which times tables do you want to work up to today?")
+                    Stepper("\(userSelection)", value: $userSelection, in: 2...12)
+                }
+                
+                Section {
+                    Text("How many questions do you want to answer?")
+                    Picker("Number of questions", selection: $selectedNumberofQuestions) {
+                        ForEach(numOfQuestions, id: \.self) { num in
+                            Text("\(num)")
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Section {
+                    List {
+                        Text("2 x 2 = ?")
+                        Text("2 x 4 = ?")
+                        Text("2 x 7 = ?")
+                        Text("2 x 9 = ?")
+                        Text("2 x 6 = ?")
+                    }
+                }
+                
+                Section {
+                    TextField("Answer", text: $userAnswer)
+                        .padding()
+                        .keyboardType(.decimalPad)
+                }
+                
+            }
+            .navigationTitle("Times Tables")
         }
-        .padding()
     }
 }
 
