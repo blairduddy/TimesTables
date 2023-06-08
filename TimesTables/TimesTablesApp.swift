7d8650e84d50ccb2f2b91d313cf99b9e49571edd
Blair Duddy <blairduddy@hotmail.co.uk>
2023-06-08 14:23:00 -0400

    Initial Commit

---
+++ TimesTables/TimesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimesTablesApp.swift
+//  TimesTables
+//
+//  Created by Blair Duddy on 2023-06-08.
+//
+
+import SwiftUI
+
+@main
+struct TimesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
